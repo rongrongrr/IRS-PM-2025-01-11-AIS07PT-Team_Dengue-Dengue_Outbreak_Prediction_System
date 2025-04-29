f2410c95c9fa22f80744fee872caeb5e5319cf3a
Velu97 <velu@lionsbot.com>
2025-04-30 01:21:49 +0800

    General cleanup of files and removal of irrelevant scripts and static htmls.

--- SystemCode/backend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate virtual environment
-source .venv/bin/activate
-
-# Run the FastAPI application with uvicorn
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
-# uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level debug
